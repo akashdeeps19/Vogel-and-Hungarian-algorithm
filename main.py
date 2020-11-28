29b3b472a9ecfc1a48b3818bd14b910f5789fb06
Sujan242 <sujanreddy242@gmail.com>
2020-11-28 14:36:04 +0530

    added random input

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import Assignment_Problem.ap as ap
 from Transportation_Problem.Russel_approx_method import Russel
 from Transportation_Problem.Vogel_approx_method import  Vogel
+import random
+import pandas as pd
+import copy
 
 if __name__ == "__main__":
 	ap.main(10)
 	print()
 	print()
-	costs  = {'W': {'A': 16, 'B': 16, 'C': 13, 'D': 22, 'E': 17},
-		  'X': {'A': 14, 'B': 14, 'C': 13, 'D': 19, 'E': 15},
-		  'Y': {'A': 19, 'B': 19, 'C': 20, 'D': 23, 'E': 50},
-		  'Z': {'A': 50, 'B': 12, 'C': 50, 'D': 15, 'E': 11}}
+
+	costs  = {'W': {'A': random.randint(10,99), 'B': random.randint(10,99), 'C': random.randint(10,99), 'D': random.randint(10,99), 'E': random.randint(10,99)},
+		  'X': {'A': random.randint(10,99), 'B': random.randint(10,99), 'C':random.randint(10,99), 'D': random.randint(10,99), 'E':random.randint(10,99)},
+		  'Y': {'A': random.randint(10,99), 'B': random.randint(10,99), 'C': random.randint(10,99), 'D': random.randint(10,99), 'E': random.randint(10,99)},
+		  'Z': {'A': random.randint(10,99), 'B': random.randint(10,99), 'C': random.randint(10,99), 'D': random.randint(10,99), 'E': random.randint(10,99)}}
 	demand = {'A': 30, 'B': 20, 'C': 70, 'D': 30, 'E': 60}
 	supply = {'W': 50, 'X': 60, 'Y': 50, 'Z': 50}
 
+	costs1=copy.deepcopy(costs)
 	print("Vogel Approximation Method")
 	obj = Vogel()
-	obj.solve(costs,demand,supply , True,False )
+	obj.solve(costs,demand,supply,True,False)
 
 	print()
 	print()
 
-	costs  = {'W': {'A': 16, 'B': 16, 'C': 13, 'D': 22, 'E': 17},
-		  'X': {'A': 14, 'B': 14, 'C': 13, 'D': 19, 'E': 15},
-		  'Y': {'A': 19, 'B': 19, 'C': 20, 'D': 23, 'E': 50},
-		  'Z': {'A': 50, 'B': 12, 'C': 50, 'D': 15, 'E': 11}}
 	demand = {'A': 30, 'B': 20, 'C': 70, 'D': 30, 'E': 60}
 	supply = {'W': 50, 'X': 60, 'Y': 50, 'Z': 50}
 	print("Russel Approximation Method")
 
 	Russel = Russel()
-	Russel.solve(costs,demand,supply,True,False)
+	Russel.solve(costs1,demand,supply,True,False)
 
 
 
